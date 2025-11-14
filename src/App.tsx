316189e7cf82553fa0992cdf1de02e9dd37a2dca
Awais Latif <97788862+MuhammmadAwais@users.noreply.github.com>
2025-11-14 20:08:41 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import gsap from 'gsap'
+import { ScrollTrigger , SplitText } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+import React from 'react'
+
+const App : React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
